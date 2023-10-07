119b0f0df9f2d9badb40cd7b71d4ffa29e6b4f53
Arthur Simoes <arthursimoes155@gmail.com>
2023-10-07 13:18:50 -0300

    :sparkles:feat: Criação do login e validações dos inputs

--- codigo-fonte/src/loginCadastro/loginCadastro.js
+++ codigo-fonte/src/loginCadastro/loginCadastro.js
@@ -1,4 +1,13 @@
-// ---- Switch code section ----
+// ---- Seção de código global ----
+
+const errorMessage = document.getElementsByClassName("error-message");
+
+function checkEmail(email) {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
+
+// ---- Seção de código do switch ----
 
 const switchCheckbox = document.querySelector(".switch-checkbox");
 const switchTextLogin = document.querySelector(".switch-text-login");
@@ -25,3 +34,39 @@ function toggleCard() {
 switchTextRegister.addEventListener("click", toggleCard);
 switchTextLogin.addEventListener("click", toggleCard);
 switchCheckbox.addEventListener("click", toggleCard);
+
+// ---- Seção de código do login ----
+
+const labelLogins = document.querySelectorAll(".label-login");
+const inputPassword = document.getElementById("password");
+const buttonLogin = document.getElementById("btn-login");
+const inputEmail = document.getElementById("email");
+
+function login() {
+  if (!checkEmail(inputEmail.value)) {
+    errorMessage[0].textContent = "Digite um e-mail válido!";
+    labelLogins[0].style.color = "red";
+    inputEmail.style.borderBottom = "0.15rem solid red";
+    setTimeout(() => {
+      errorMessage[0].textContent = "";
+      labelLogins[0].style.color = "#3c615c";
+      inputEmail.style.borderBottom = "0.15rem solid #3c615c";
+    }, 3000);
+    return;
+  }
+  if (inputPassword.value === "") {
+    errorMessage[0].textContent = "Digite sua senha!";
+    labelLogins[1].style.color = "red";
+    inputPassword.style.borderBottom = "0.15rem solid red";
+    setTimeout(() => {
+      errorMessage[0].textContent = "";
+      labelLogins[1].style.color = "#3c615c";
+      inputPassword.style.borderBottom = "0.15rem solid #3c615c";
+    }, 3000);
+    return;
+  }
+
+  alert(`Email: ${inputEmail.value}, Senha: ${inputPassword.value}`);
+}
+
+buttonLogin.addEventListener("click", login);

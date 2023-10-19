b8cb2683dc27d8fd73efc870158c5c250b33baea
Aleksander Romero <aleksandercgr@gmail.com>
2023-10-19 19:28:41 -0300

    :sparkles: feat: Correcao bug navbar tela projetos

---
+++ codigo-fonte/src/projetos/menu.js
@@ -0,0 +1,11 @@
+const menuButton = document.querySelector('.navbar-toggler');
+const mainContent = document.getElementById('mainContent');
+console.log(menuButton);
+console.log(mainContent);
+menuButton.addEventListener('click', () => {
+    mainContent.classList.toggle('margin-adjust');
+
+    if (!mainContent.classList.contains('margin-adjust')) {
+        mainContent.style.transition = 'margin 0.5s ease-out';
+    }
+});

ee827c51e5116c59f7c91a14e8ea24ca301c81be
Átila Ribeiro <atilaedu1@gmail.com>
2023-11-17 14:40:10 -0300

    Botões e novas imagens de perfil para voluntário.

--- codigo-fonte/src/perfilVoluntario/perfilVoluntarioo.js
+++ codigo-fonte/src/perfilVoluntario/perfilVoluntarioo.js
@@ -24,3 +24,48 @@ function finalizar() {
         });
     });
 }
+
+
+
+const imageButton = document.querySelector('.btn-imagem-perfil');
+const backButton = document.querySelector('.btn-back');
+const nextButton = document.querySelector('.btn-next');
+const perfilImagem = document.querySelector('.perfil');
+
+const images = [
+    '../homePage/images/perfil-1.jpeg',
+    './perfilVoluntario/imagens/perfil/h1.jpg',
+    './perfilVoluntario/imagens/perfil/j1.jpg',
+    './perfilVoluntario/imagens/perfil/j2.jpg',
+    './perfilVoluntario/imagens/perfil/j3.jpg',
+    './perfilVoluntario/imagens/perfil/m1.jpg',
+    './perfilVoluntario/imagens/perfil/m2.jpg',
+    './perfilVoluntario/imagens/perfil/v1.jpg',
+    './perfilVoluntario/imagens/perfil/v2.jpg',
+];
+
+
+let atualIndex = 0;
+
+imageButton.addEventListener('click', () => {
+    backButton.style.display = 'inline-block';
+    nextButton.style.display = 'inline-block';
+    perfilImagem.src = images[atualIndex];
+});
+
+
+
+backButton.addEventListener('click' , () => {
+    atualIndex = (atualIndex - 1 + images.length) % images.length;
+    perfilImagem.src = images[atualIndex];
+});
+
+nextButton.addEventListener('click' , () => {
+    atualIndex = (atualIndex + 1) % images.length;
+    perfilImagem.src = images[atualIndex];
+});
+
+
+document.getElementById("emitirCertificadoBtn").addEventListener('click', function(){
+    window.open('../certificacao/certificacao.html', '_blank')
+})

e853fa51b7a2bed6eab1ef9e0d714183c5bc8f25
Gabriela Araujo <gabriela.araujo.1486579@sga.pucminas.br>
2023-11-19 16:51:31 -0300

    :sparkles: uptade: js_contagem do volume de projetos na tela

--- codigo-fonte/src/perfilEmpresa/perfilEmpresa.js
+++ codigo-fonte/src/perfilEmpresa/perfilEmpresa.js
@@ -14,5 +14,25 @@ document.querySelectorAll('.btn-secondary').forEach((button) => {
         const projetoCard = button.closest('.card-projetos');
 
         projetoCard.classList.add('d-none');
+
+        // Atualiza o número total de projetos subtraindo 1
+        updateTotalProjetos();
     });
 });
+
+function updateTotalProjetos() {
+    // Obtém o elemento que exibe o número total de projetos
+    const totalProjetosElement = document.getElementById('totalProjetos');
+
+    // Obtém o número atual de projetos
+    let totalProjetos = parseInt(totalProjetosElement.textContent);
+
+    // Subtrai 1 do número total de projetos
+    totalProjetos--;
+
+    // Atualiza o texto com o novo número total de projetos
+    totalProjetosElement.textContent = totalProjetos;
+
+    // Adiciona um log para verificar se a função está sendo chamada e o novo valor
+    console.log('Botão "Encerrar" clicado. Atualizando total de projetos.', totalProjetos);
+}

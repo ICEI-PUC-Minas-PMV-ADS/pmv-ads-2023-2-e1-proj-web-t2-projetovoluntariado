d2d1d68c9d8a3a67ad84eabf1d73b172376c59a2
dinhoop <anderson@digitallone.net>
2023-11-21 16:37:25 -0300

    feat: listar projetos do perfil empresa - inicio

--- codigo-fonte/src/perfilEmpresa/perfilEmpresa.js
+++ codigo-fonte/src/perfilEmpresa/perfilEmpresa.js
@@ -34,3 +34,34 @@ function updateTotalProjetos() {
 
     console.log('Botão "Encerrar" clicado. Atualizando total de projetos.', totalProjetos);
 }
+function listarProjetosEmpresa(){
+    let users = getUsers();
+    let userLogged = isLogged()
+    let projects =[];
+    users.forEach((user)=>{
+        if(user.email === userLogged.email){
+            projects = user.projects;
+
+            //Continuar...
+        }
+    });
+
+}
+function createCards(project, id){
+    let div = document.createElement("div");
+    div.setAttribute("class", "card rounded-5 card-projetos border-0");
+    let img = document.createElement("img");
+    img.setAttribute("class", "mx-auto mb-4 img-fluid perfil rounded-5");
+    img.setAttribute("src", project.imgLink);
+    let h4 = document.createElement("h4");
+    h4.textContent = project.projectName;
+    let h5 = document.createElement("h5");
+    h5.textContent = project.availability;
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "col text-center margin");
+    let button = document.createElement("button");
+    button.setAttribute("class","btn btn-secondary my-2");
+    button.setAttribute("id", "encerrarBtn1");
+
+    //continuar com appendChild
+}

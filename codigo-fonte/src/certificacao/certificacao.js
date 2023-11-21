3574e8fae4cddfbbc6e751ea1a95d2b5db2d6117
Átila Ribeiro <atilaedu1@gmail.com>
2023-11-20 21:02:45 -0300

    update: certificado

--- codigo-fonte/src/certificacao/certificacao.js
+++ codigo-fonte/src/certificacao/certificacao.js
@@ -16,15 +16,55 @@ function certificadoVar() {
     let usernameCertificado = isLogged();
     let horasCertificado = getHours();
 
+
     console.log(usernameCertificado)
     console.log(horasCertificado)
-    
+
 
 
     document.getElementById("username").textContent = usernameCertificado.name
+    document.getElementById("username2").textContent = usernameCertificado.name
     document.getElementById("horasCertificado").textContent = horasCertificado
+    document.getElementById("nivelName").textContent = getNivel(horasCertificado)
+    document.getElementById("nivelName2").textContent = getNivel(horasCertificado)
 
 }
+
+
+function getNivel(horas) {
+    if (horas > 2 && horas < 10) {
+        return "Safira";
+    } else if (horas >= 10 && horas < 30) {
+        return "Ametista";
+    } else if (horas >= 30 && horas < 60) {
+        return "Esmeralda";
+    } else if (horas <= 60) {
+        return "Vazio";
+    } else {
+        return "Rubi";
+    }
+}
+
+
+function iconeCertificado() {
+    let horas = getHours()
+    if (horas > 2 && horas < 10) {
+        document.getElementById("imagemNivel").src ="../perfilVoluntario/imagens/esmeralda.png";
+    } else if (horas >= 10 && horas < 30) {
+        document.getElementById("imagemNivel").src ="../perfilVoluntario/imagens/esmeralda.png";
+    } else if (horas >= 30 && horas < 60) {
+        document.getElementById("imagemNivel").src ="../perfilVoluntario/imagens/esmeralda.png";
+    } else if(horas <= 60){
+        document.getElementById("imagemNivel").src ="../perfilVoluntario/imagens/vazio.png";
+    } else {
+        document.getElementById("imagemNivel").src ="../perfilVoluntario/imagens/rubi.png";
+
+    }
+
+
+}
+
+iconeCertificado()
 
 
 
